8b1acb346b1a519a80f4d3a033901c9255cfe6b5
Abhishek Mishra <abhishek.mishra@zopsmart.com>
2024-07-10 11:44:40 +0530

    Added search

--- MyContacts/ViewController.swift
+++ MyContacts/ViewController.swift
@@ -11,16 +11,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var sectionTitle = [String]()
     
     var namesDict = [String: [String]]()
     
     var dummyData =  [String]()
     
+    var finalData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        searchBar.delegate = self
         let path = Bundle.main.path(forResource: "Name", ofType: "json")
         let url = URL(fileURLWithPath: path!)
         do{
@@ -32,7 +37,9 @@ class ViewController: UIViewController {
         }
         catch{}
         
-        sectionTitle = Array(Set(dummyData.compactMap({
+        finalData = dummyData
+        
+        sectionTitle = Array(Set(finalData.compactMap({
             String($0.prefix(1))
             })))
         
@@ -42,11 +49,42 @@ class ViewController: UIViewController {
             namesDict[title] = [String]()
         }
         
-        for name in dummyData{
+        for name in finalData{
             namesDict[String(name.prefix((1)))]?.append(name)
         }
         
-        print(namesDict)
+        //print(namesDict)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            finalData = dummyData
+        } else {
+            finalData = dummyData.filter {
+                $0.lowercased().contains(searchText.lowercased())
+            }
+        }
+
+        sectionTitle = Array(Set(finalData.compactMap {
+            String($0.prefix(1))
+        }))
+        
+        sectionTitle.sort()
+        
+        namesDict.removeAll()
+        for title in sectionTitle {
+            namesDict[title] = [String]()
+        }
+        
+        for name in finalData {
+            namesDict[String(name.prefix(1))]?.append(name)
+        }
+        
+        print("Filtered Final Data:", finalData)
+        print("Filtered Names Dict:", namesDict)
+        tableView.reloadData()
     }
 }
 
